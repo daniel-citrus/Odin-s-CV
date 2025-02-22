56de4659efdfbbb662a46b9ae431347593fc4d15
Daniel <dandcalvo@gmail.com>
2025-02-21 23:47:29 -0800

    edit and cancel function

--- src/components/inputSections/EducationalInput.jsx
+++ src/components/inputSections/EducationalInput.jsx
@@ -4,18 +4,28 @@ import { v4 as uuid4 } from 'uuid';
 import InputListControls from './inputControls/InputListControls';
 
 export default function EducationalInput() {
+<<<<<<< HEAD
     const { educationData, setEducationData } = useContext(DataContext);
+=======
+    const { data: educationData, setData: setEducationData } =
+        useContext(DataContext);
+>>>>>>> 4c08896 (edit and cancel function)
 
     const [school, setSchool] = useState('');
     const [study, setStudy] = useState('');
     const [studyStartDate, setStudyStartDate] = useState('');
     const [studyEndDate, setStudyEndDate] = useState('');
+<<<<<<< HEAD
 
     const [editing, setEditing] =
         useState(null); /* ID of educationData element that is being edited */
     const [prevData, setPrevData] = useState(
         {}
     ); /* Remember previous data for cancelled edits */
+=======
+    const [editing, setEditing] = useState(null);
+    const [prevData, setPrevData] = useState({});
+>>>>>>> 4c08896 (edit and cancel function)
 
     const handleChange = (e) => {
         const target = e.target;
@@ -75,7 +85,6 @@ export default function EducationalInput() {
 
     const handleEdit = (id) => {
         const data = educationData.find((d) => d.id === id);
-        setEditing(true);
 
         if (data === undefined) {
             return;
@@ -109,12 +118,32 @@ export default function EducationalInput() {
                     return (
                         <li className='educationDataListItem' key={d.id}>
                             {d.school}
+<<<<<<< HEAD
                             <InputListControls
                                 editing={editing}
                                 id={d.id}
                                 handleEdit={() => handleEdit(d.id)}
                                 handleDelete={() => handleDelete(d.id)}
                             />
+=======
+                            <button
+                                type='button'
+                                className={
+                                    editing === d.id ? 'bg-gray-700' : ''
+                                }
+                                disabled={editing === d.id}
+                                onClick={() => handleEdit(d.id)}
+                            >
+                                Edit
+                            </button>
+
+                            <button
+                                type='button'
+                                onClick={() => handleDelete(d.id)}
+                            >
+                                Delete
+                            </button>
+>>>>>>> 4c08896 (edit and cancel function)
                         </li>
                     );
                 })}
